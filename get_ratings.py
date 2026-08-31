e5a4adde0f50ec4aeee52832c8b0abfe2cb87a60
Pranav Mandala <pranav.mandala07@gmail.com>
2026-08-31 16:37:08 -0700

    finished mapping

--- get_ratings.py
+++ get_ratings.py
@@ -1,34 +1,70 @@
 import yfinance as yf
 import pandas as pd
 
-ticker = yf.Ticker("META")
-events = ticker.upgrades_downgrades
-df = pd.DataFrame(events)
-
-actions = {
-    "Overweight" : "buy",
-    "Outperform" : "buy",
-    "Buy" : "buy",
-    "Strong Buy" : "buy",
-    "Positive" : "buy",
-    "Market Outperform" : "buy",
-    "Sector Outperform" : "buy",
-    "Neutral" : "hold",
-    "Hold": "hold",
-    "Market Perform" : "hold",
-    "Equal-Weight" : "hold",
-    "Perform" : "hold",
-    "Peer Perform" : "hold",
-    "Sector Weight" : "hold",
-    "Sector Perform" : "hold",
-    "Underperform" : "sell",
-    "Underweight" : "sell",
-    "Reduce" : "sell",
-    "Sell" : "sell"
+stocks = {
+    "Nvidia" : "NVDA",
+    "Apple" : "AAPL",
+    "Microsoft" : "MSFT",
+    "AMD" : "AMD",
+    "Intel" : "INTC",
+    "Cisco" : "CSCO",
+    "Oracle" : "ORCL",
+    "IBM" : "IBM",
+    "Adobe" : "ADBE",
+    "Salesforce" : "CRM",
+    "Uber" : "UBER",
+    "JP Morgan" : "JPM",
+    "Visa" : "V",
+    "Mastercard" : "MA",
+    "Wells Fargo" : "WFC",
+    "Morgan Stanley" : "MS",
+    "American Express" : "AXP",
+    "Bank of America" : "BAC",
+    "Amazon" : "AMZN",
+    "Tesla" : "TSLA",
+    "Meta" : "Meta",
+    "Google" : "Googl",
+    "Netflix" : "NFLX",
+    "Disney" : "DIS",
+    "Verizon" : "VZ",
+    "Costco" : "COST",
+    "Sandisk" : "SNDK",
+    "HP" : "HPE",
+    "Motorola" : "MSI",
+    "Airbnb" : "ABNB",
+    "DoorDash" : "DASH"
 }
 
-for i in df['ToGrade']:
-    if i in actions:
-        continue
-    else:
-        print(i)
+actions = {
+    "Overweight": "buy",
+    "Outperform": "buy",
+    "Buy": "buy",
+    "Strong Buy": "buy",
+    "Positive": "buy",
+    "Market Outperform": "buy",
+    "Sector Outperform": "buy",
+    "Accumulate": "buy",
+    "Outperformer": "buy",
+    "Top Pick": "buy",
+    "Long-Term Buy": "buy",
+    "Neutral": "hold",
+    "Hold": "hold",
+    "Market Perform": "hold",
+    "Equal-Weight": "hold",
+    "Equal-weight": "hold",
+    "Perform": "hold",
+    "Peer Perform": "hold",
+    "Sector Weight": "hold",
+    "Sector Perform": "hold",
+    "Fair Value": "hold",
+    "Average": "hold",
+    "In-Line": "hold",
+    "Market Weight": "hold",
+    "Mixed": "hold",
+    "Underperform": "sell",
+    "Underweight": "sell",
+    "Reduce": "sell",
+    "Sell": "sell",
+    "Market Underperform": "sell",
+    "Negative": "sell",
+}
